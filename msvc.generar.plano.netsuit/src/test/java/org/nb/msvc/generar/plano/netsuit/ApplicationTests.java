d1e8f81f866bc945f0b9bc4197713f5fe9518bb7
dvwilmertorres <dv.wilmer.torres@outlook.com>
2023-01-09 12:57:40 -0500

    Creacion proyecto modular

---
+++ msvc.generar.plano.netsuit/src/test/java/org/nb/msvc/generar/plano/netsuit/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nb.msvc.generar.plano.netsuit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
